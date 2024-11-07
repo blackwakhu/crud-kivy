0bcee5fd5157164cff072d42936b2a2d90575856
Derrick wakhu shibero <shiberoderrickwakhu@gmail.com>
2024-11-07 13:01:30 +0300

    first run that worked

--- form/mainform.py
+++ form/mainform.py
@@ -1,5 +1,7 @@
 from kivy.app import App
+from kivy.uix.label import Label
 
 class MainFormApp(App):
     def build(self):
-        pass
+        l1 = Label(text="Hello World", font_size=50)
+        return l1
