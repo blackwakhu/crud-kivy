d1c0d6c67aa2251132db40a7381d1393695aa75f
Derrick wakhu shibero <shiberoderrickwakhu@gmail.com>
2024-11-07 12:48:30 +0300

    added class MyLayout

--- form/mainform.py
+++ form/mainform.py
@@ -1,6 +1,10 @@
 from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+
+class MyLayout(BoxLayout):
+    pass
 
 
 class MainFormApp(App):
     def build(self):
-        pass
+        return MyLayout()
