789adf8a0854258da1a965f419c12768a56c0e7a
Derrick wakhu shibero <shiberoderrickwakhu@gmail.com>
2024-11-07 12:59:31 +0300

    starting again with a tutorial

--- form/mainform.py
+++ form/mainform.py
@@ -1,10 +1,5 @@
 from kivy.app import App
-from kivy.uix.boxlayout import BoxLayout
-
-class MyLayout(BoxLayout):
-    pass
-
 
 class MainFormApp(App):
     def build(self):
-        return MyLayout()
+        pass
